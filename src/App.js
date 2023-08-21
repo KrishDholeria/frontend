a93a6cf1f750646a4ce7f48e3d4488db137c8701
Krish Dholeria <krishdholeria1@gmail.com>
2023-08-21 23:47:49 +0530

    21 Aug final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [notes, setNotes] = useState(null);
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  const fetchNotes = async () => {
+    // fetch the notes
+    const res = await axios.get('http://localhost:3000/notes');
+    // set on state
+    setNotes(res.data.notes);
+  }
   return (
     <div className="App">
-      hello world
+      <div>
+        <h2>Notes:</h2>
+        {notes &&
+          notes.map((note) =>{
+            return (
+              <div key={note.id}>
+                <h3>{note.title}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
